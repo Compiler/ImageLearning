1b44f308d10f11f2afd974ccc39a8fb780b6327d
luke roche <lukerochedev@gmail.com>
2017-11-13 05:45:12 -0500

    matches handwritten numbers to digits

--- core.py
+++ core.py
@@ -5,4 +5,12 @@ from sklearn import svm
 
 digits = datasets.load_digits()
 
-print(digits.data)
+clf = svm.SVC(gamma = 0.001, C=100)
+
+x,y = digits.data[:-1], digits.target[:-1]
+clf.fit(x,y)
+data = digits.data[-1].reshape(1,-1)
+print('prediction:', clf.predict(data))
+
+plt.imshow(digits.images[-1], cmap = plt.cm.gray_r, interpolation="nearest")
+plt.show()
